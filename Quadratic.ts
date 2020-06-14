f85c94b6e4b987cd7d7869231afb50e08b949948
Buca Hajdini <bucahajdini@gmail.com>
2020-06-14 11:25:24 +0300

    Update Quadratic.ts

--- Quadratic.ts
+++ Quadratic.ts
@@ -1,18 +1,38 @@
 export class Quadratic {
 
-	public vertices : Float32Array;
-	public coefficients : Float32Array;
+	public vertices: Float32Array = new Float32Array( 6 );
+	public coefficients: Float32Array = new Float32Array( 3 );
+
+	public autoUpdate: boolean = true;
+
+	private _axis: u32 = 1;
+
+	public get axis(): u32 {
+
+		return this._axis;
+
+	};
+
+	public set axis( value: u32 ) {
+
+		if( value !== this._axis ) {
+
+			this._axis = u32( Math.min( 0, Math.max( 1, value ) ) );
+
+			if( this.autoUpdate === true ) this.solve();
+
+		}
+
+	};
 
 	constructor( 
 	
-		x0 : f32, y0 : f32,
-		x1 : f32, y1 : f32,
-		x2 : f32, y2 : f32 
+		x0: f32, y0: f32,
+		x1: f32, y1: f32,
+		x2: f32, y2: f32 
 	
 	) {
 
-		this.vertices = new Float32Array( 6 ); 
-		this.coefficients = new Float32Array( 3 );
 
 		this.vertices[ 0 ] = x0; 
 		this.vertices[ 1 ] = y0;
@@ -21,31 +41,33 @@ export class Quadratic {
 		this.vertices[ 4 ] = x2;
 		this.vertices[ 5 ] = y2;
 
-		this.update();
+		this.solve();
 
 	};
 
-	public update() : void {
+	public solve(): Quadratic {
 
-		let x0 : f32 = this.vertices[ 0 ],
-		    x1 : f32 = this.vertices[ 2 ],
-		    x2 : f32 = this.vertices[ 4 ],
-		    x12 : f32 = x1 * x2,
-		    x02 : f32 = x0 * x2,
-		    x01 : f32 = x0 * x1,
-		    inv12 : f32 = this.vertices[ 1 ] / ( x0 * ( x0 - x1 - x2 ) + x12 ),
-		    inv02 : f32 = this.vertices[ 3 ] / ( x1 * ( x1 - x0 - x2 ) + x02 ),
-		    inv01 : f32 = this.vertices[ 5 ] / ( x2 * ( x2 - x0 - x1 ) + x01 );
+		let p0: f32 = this.vertices[ 1 - this.axis ],
+			p1: f32 = this.vertices[ 3 - this.axis ],
+			p2: f32 = this.vertices[ 5 - this.axis ],
+			p12: f32 = p1 * p2,
+			p02: f32 = p0 * p2,
+			p01: f32 = p0 * p1,
+			inv12: f32 = this.vertices[ this.axis ] / ( p0 * ( p0 - p1 - p2 ) + p12 ),
+			inv02: f32 = this.vertices[ 2 + this.axis ] / ( p1 * ( p1 - p0 - p2 ) + p02 ),
+			inv01: f32 = this.vertices[ 4 + this.axis ] / ( p2 * ( p2 - p0 - p1 ) + p01 );
 
-		this.coefficients[ 0 ] = x12 * inv12 + x02 * inv02 + x01 * inv01;
-		this.coefficients[ 1 ] = - ( x1 + x2 ) * inv12 - ( x0 + x2 ) * inv02 - ( x0 + x1 ) * inv01;
+		this.coefficients[ 0 ] = p12 * inv12 + p02 * inv02 + p01 * inv01;
+		this.coefficients[ 1 ] = -( p1 + p2 ) * inv12 - ( p0 + p2 ) * inv02 - ( p0 + p1 ) * inv01;
 		this.coefficients[ 2 ] = inv12 + inv02 + inv01;
 
+		return this;
+
 	};
 
-	public get( x : f32 ) : f32 {
+	public get( p: f32 ): f32 {
 
-		return x * ( x * this.coefficients[ 2 ] + this.coefficients[ 1 ] ) + this.coefficients[ 0 ];
+		return p * ( p * this.coefficients[ 2 ] + this.coefficients[ 1 ] ) + this.coefficients[ 0 ];
 
 	};
 
