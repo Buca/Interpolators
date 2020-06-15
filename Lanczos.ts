23d7b59912b09a759d02e0970119ba701fbe4780
Buca Hajdini <bucahajdini@gmail.com>
2020-06-15 13:56:00 +0300

    Update Lanczos.ts

--- Lanczos.ts
+++ Lanczos.ts
@@ -51,14 +51,14 @@ export class Lanczos {
 
 	public solve(): Lanczos {
 
-		//let x0: f32 = this.vertices[ 1 - this.axis ],
-		//	y0: f32 = this.vertices[ 0 + this.axis ],
-		//	x1: f32 = this.vertices[ 3 - this.axis ],
-		//	y1: f32 = this.vertices[ 2 + this.axis ],
-		//	inverse_d: f32 = 1 / ( x1 - x0 );
+		let x0: f32 = this.vertices[ 1 - this.axis ],
+			y0: f32 = this.vertices[ 0 + this.axis ],
+			x1: f32 = this.vertices[ 3 - this.axis ],
+			y1: f32 = this.vertices[ 2 + this.axis ],
+			inverse_d: f32 = 1 / ( x1 - x0 );
 
-		//this.coefficients[ 0 ] = inverse_d * ( x1 * y0 - x0 * y1 );
-		//this.coefficients[ 1 ] = inverse_d * ( y1 - y0 );
+		this.coefficients[ 0 ] = x0 * inverse_d;
+		this.coefficients[ 1 ] = inverse_d;
 
 		return this;
 
@@ -66,28 +66,25 @@ export class Lanczos {
 
 	public get( p: f32 ): f32 {
 
-		let x0: f32 = this.vertices[ 0 ],
-			y0: f32 = this.vertices[ 1 ],
-			x1: f32 = this.vertices[ 2 ],
-			y1: f32 = this.vertices[ 3 ],
-			x: f32 = ( p - x0 ) / ( x1 - x0 );
+		let y0: f32 = this.vertices[ 0 + this.axis ],
+			y1: f32 = this.vertices[ 2 + this.axis ],
+			x: f32 = this.coefficients[ 1 ] * p + this.coefficients[ 0 ];
 
-		return y0 * lanczosKernel( this.window, ( p - x0 ) / ( x1 - x0 ) ) + y1 * lanczosKernel( this.window, ( p - x0 ) / ( x1 - x0 ) - 1 );
+		if( x === 0 ) return 1;
+
+		else if( -this.window <= x && x < this.window ) {
+
+			return this.window * ( 
+			
+				  y0 * Mathf.sin( Mathf.PI * x ) * Mathf.sin( Mathf.PI * x / this.window ) / Mathf.pow( Mathf.PI * x, 2 )
+				+ y1 * Mathf.sin( Mathf.PI * ( x - 1 ) ) * Mathf.sin( Mathf.PI * ( x - 1 ) / this.window ) / Mathf.pow( Mathf.PI * ( x - 1 ), 2 ) 
+			
+			);
+
+		}
+	
+		else return 0;
 
 	};
-
-};
-
-function lanczosKernel( window: f32, x: f32 ):f32 {
-
-	if( x === 0 ) return 1;
-
-	else if( -range <= x && x < range ) {
-
-		return window * Mathf.sin( Mathf.PI * x ) * Mathf.sin( Mathf.PI * x / window ) / Mathf.pow( Mathf.PI * x, 2 );
-
-	}
-	
-	else return 0;
 
 };
